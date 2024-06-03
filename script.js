316af8ae6ab49ed07eed6588b58188384e9ad6f1
Diego Aveiro <127653491+diegoaveiro@users.noreply.github.com>
2024-06-03 18:44:04 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 AOS.init({
-  anchorPlacement: 'top-bottom',
-  offset: 200,
+  anchorPlacement: 'center-center',
+  offset: 100,
   duration: 1200,
   mirror: true,
   once: false,
   useClassNames: true,
   debounceDelay: 200,
 })
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const menuBtn = document.querySelector('.menu-btn');
+//   const menuItem = document.querySelector('.menu-item');
+
+//   menuBtn.addEventListener('click', function() {
+//       menuItem.classList.toggle('expanded');
+//   });
+// });
+
+
+
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
